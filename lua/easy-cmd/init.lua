8fde987fc80b28ab7bce9de325d81cffc38fee4d
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-12-10 14:08:06 -0300

    feat: run visual selection

--- lua/easy-cmd/init.lua
+++ lua/easy-cmd/init.lua
@@ -10,9 +10,32 @@ local function output_to_buf(bufnr)
   end
 end
 
+local function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+
+  if lines[1] == nil then
+    return {}
+  end
+
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return lines
+end
+
 M.run_command = function()
   local bufnr = vim.api.nvim_get_current_buf()
+
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, nil)
+  if get_visual_selection() ~= {} then
+    lines = get_visual_selection()
+  end
 
   local win_info = ui.open_window()
 

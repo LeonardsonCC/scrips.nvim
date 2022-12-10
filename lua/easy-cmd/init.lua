c31444bbd1ef04ca17bb94a76dc0687545bc7ce9
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-12-10 13:53:50 -0300

    refactor: private function output_to_buf

--- lua/easy-cmd/init.lua
+++ lua/easy-cmd/init.lua
@@ -2,6 +2,14 @@ local ui = require('easy-cmd.ui')
 
 M = {}
 
+local function output_to_buf(bufnr)
+  return function(_, data)
+    if data then
+      vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    end
+  end
+end
+
 M.run_command = function()
   local bufnr = vim.api.nvim_get_current_buf()
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, nil)
@@ -17,17 +25,14 @@ M.run_command = function()
 
   vim.fn.jobstart(cmd, {
     stdout_buffered = true,
-    on_stdout = M.output_to_buf(win_info.bufnr),
-    on_stderr = M.output_to_buf(win_info.bufnr),
-  })
-end
-
-M.output_to_buf = function(bufnr)
-  return function(_, data)
-    if data then
-      vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    on_stdout = output_to_buf(win_info.bufnr),
+    on_stderr = output_to_buf(win_info.bufnr),
+    on_exit = function(_, _)
+      vim.api.nvim_buf_set_lines(win_info.bufnr, -1, -1, false, {
+        "Done!",
+      })
     end
-  end
+  })
 end
 
 return M

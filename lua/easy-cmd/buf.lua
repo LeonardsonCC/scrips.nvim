691c40608d3344d5af7c42928c7181a1ef1a0b31
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-12-10 19:54:14 -0300

    feat: create new script

---
+++ lua/easy-cmd/buf.lua
@@ -0,0 +1,26 @@
+function Output_to_buf(bufnr)
+  return function(_, data)
+    if data then
+      vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    end
+  end
+end
+
+function Get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+
+  if #lines == 0 then
+    return {}
+  end
+
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return lines
+end
--- lua/easy-cmd/fs.lua
+++ lua/easy-cmd/fs.lua
@@ -1,11 +1,33 @@
 local Path = require('plenary.path')
 
-local M = {}
+local M = {
+  path = "",
+}
 
 M.setup_folder = function(path)
   local expanded_path = vim.fn.expand(path)
 
+  M.path = path
+
   Path:new(expanded_path):mkdir({})
+end
+
+local function new_file(name)
+  local expanded_file = vim.fn.expand(M.path .. name)
+  local file = Path:new(expanded_file)
+  if not file:exists() then
+    file:write("", "w")
+  end
+end
+
+local function open_file(name)
+  local expanded_file = vim.fn.expand(M.path .. name)
+  vim.cmd('e ' .. expanded_file)
+end
+
+M.open_or_create_file = function(name)
+  new_file(name)
+  open_file(name)
 end
 
 return M
